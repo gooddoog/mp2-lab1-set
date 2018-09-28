b0828086a5cfcaca151e573d229fc7a8473f478c
Arseniy Obolenskiy <gooddoog@student.su>
2018-09-28 09:54:27 +0300

    Fixed clang incompatibility

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -18,7 +18,9 @@ TBitField::TBitField(int len)
 		throw 1;
 	BitLen = len;
 	MemLen = (len + sizeof(TELEM) * 8 - 1) / (sizeof(TELEM) * 8);
-	pMem = new TELEM[MemLen] {0};
+	pMem = new TELEM[MemLen];
+	for (int i = 0; i < MemLen; ++i)
+        pMem[i] = 0;
 }
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
